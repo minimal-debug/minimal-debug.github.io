c23ae6df7ffce949f48fa05c11405ee895ad9651
Aldi Piroli <aldipiroli@gmail.com>
2025-08-17 07:29:37 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,31 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://minimal-debug.github.io'
+	site: 'https://minimal-debug.github.io/',
+	integrations: [
+		starlight({
+			title: 'minimal-debug',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/aldipiroli' }, { icon: 'youtube', label: 'YouTube', href: 'https://www.youtube.com/@MinimalDebug' }],
+			sidebar: [
+				{
+					label: 'Explained Papers',
+					autogenerate: { directory: 'papers' },
+
+				},
+				{
+					label: 'Projects',
+					autogenerate: { directory: 'projects' },
+				},
+				{
+					label: 'About',
+					autogenerate: { directory: 'about' },
+				},
+			],
+			pagination: false,
+			lastUpdated: false
+		}),
+	],
 });
---
+++ src/content/docs/about/about.md
@@ -0,0 +1,10 @@
+---
+title: About
+description: A guide in my new Starlight docs site.
+---
+## About Us
+Minimal-Debug aims to provide high-quality educational material covering various aspects of deep learning.
+
+
+## Contacts
+Contact us at: `minimaldebug at protonmail dot com`
